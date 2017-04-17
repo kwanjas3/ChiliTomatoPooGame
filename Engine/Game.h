4a1673f9604d20f0f55f837fafe0ce03c4eed435
kwanjas3 <kwanjas0412@gmail.com>
2017-04-17 00:25:48 -0400

    first game

---
+++ Engine/Game.h
@@ -0,0 +1,98 @@
+/****************************************************************************************** 
+ *	Chili DirectX Framework Version 16.07.20											  *	
+ *	Game.h																				  *
+ *	Copyright 2016 PlanetChili.net <http://www.planetchili.net>							  *
+ *																						  *
+ *	This file is part of The Chili DirectX Framework.									  *
+ *																						  *
+ *	The Chili DirectX Framework is free software: you can redistribute it and/or modify	  *
+ *	it under the terms of the GNU General Public License as published by				  *
+ *	the Free Software Foundation, either version 3 of the License, or					  *
+ *	(at your option) any later version.													  *
+ *																						  *
+ *	The Chili DirectX Framework is distributed in the hope that it will be useful,		  *
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of						  *
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  *
+ *	GNU General Public License for more details.										  *
+ *																						  *
+ *	You should have received a copy of the GNU General Public License					  *
+ *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
+ ******************************************************************************************/
+#pragma once
+
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "Graphics.h"
+
+class Game
+{
+public:
+	Game( class MainWindow& wnd );
+	Game( const Game& ) = delete;
+	Game& operator=( const Game& ) = delete;
+	void Go();
+private:
+	void ComposeFrame();
+	void UpdateModel();
+	/********************************/
+	/*  User Functions              */
+   void DrawFace(int x, int y );
+   void DrawPoo(int x, int y);
+   void GameOver(int x, int y);
+   void DrawTitle(int x ,int y);
+   int ClampScreenX(int x, int width)
+   {
+      const int right = x + width;
+      if (x < 0) {
+      return 0;
+      }
+      else if (right >= gfx.ScreenWidth) {
+         return (gfx.ScreenWidth - 1) - width;
+      }
+      else {
+         return x;
+      }
+   }
+   int ClampScreenY(int y, int height)
+   {
+      const int bottom = y + height;
+      if (y < 0) {
+      return 0;
+      }
+      else if (bottom >= gfx.ScreenHeight) {
+         return (gfx.ScreenHeight - 1) - height;
+      }
+      else
+         return y;
+   }
+   bool isColliding(int x0, int y0, int width0, int height0, 
+      int x1, int y1, int width1, int height1);
+	/********************************/
+private:
+	MainWindow& wnd;
+	Graphics gfx;
+	/********************************/
+	/*  User Variables              */
+   int dudeX = 400;
+   int dudeY = 300;
+   int dudeWidth = 20;
+   int dudeHeight = 20;
+
+   int pooWidth = 24;
+   int pooHeight = 24;
+
+   int poo0x = 300;
+   int poo0y = 150;
+   bool poo0IsEaten = false;
+
+   int poo1x = 100;
+   int poo1y = 500;
+   bool poo1IsEaten = false;
+
+   int poo2x = 600;
+   int poo2y = 400;
+   bool poo2IsEaten = false;
+
+   bool isStarted = false;
+	/********************************/
+};
